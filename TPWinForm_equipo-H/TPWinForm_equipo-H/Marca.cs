ea680917e769d1fad2698ffade1fcc5fc79072eb
JuanMVela <juanmavela04@gmail.com>
2026-09-09 14:57:49 -0300

    Agregar clase Marca

---
+++ TPWinForm_equipo-H/TPWinForm_equipo-H/Marca.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPWinForm_equipo_H
+{
+    internal class Marca
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
