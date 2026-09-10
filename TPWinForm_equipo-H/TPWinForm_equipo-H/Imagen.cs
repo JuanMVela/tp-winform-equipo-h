69b68c83b972cc896839cfb9d00355e794d7b515
JuanMVela <juanmavela04@gmail.com>
2026-09-10 13:54:58 -0300

    Clase Imagen

---
+++ TPWinForm_equipo-H/TPWinForm_equipo-H/Imagen.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPWinForm_equipo_H
+{
+    internal class Imagen
+    {
+        public int Id { get; set; }
+        public int IdArticulo { get; set; }
+        public string ImagenUrl { get; set; }
+    }
+}
