4a712ab766b8384215e18a711b45d89243440050
JuanMVela <juanmavela04@gmail.com>
2026-09-10 14:46:01 -0300

    Se renombro Form1 por frmPrincipal

--- TPWinForm_equipo-H/TPWinForm_equipo-H/Articulo.cs
+++ TPWinForm_equipo-H/TPWinForm_equipo-H/Articulo.cs
@@ -6,5 +6,13 @@ namespace TPWinForm_equipo_H
 {
     internal class Articulo
     {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public decimal Precio { get; set; }
+        public List<Imagen> Imagenes { get; set; }
     }
 }
--- TPWinForm_equipo-H/TPWinForm_equipo-H/frmPrincipal.cs
+++ TPWinForm_equipo-H/TPWinForm_equipo-H/frmPrincipal.cs
@@ -1,8 +1,8 @@
 namespace TPWinForm_equipo_H
 {
-    public partial class Form1 : Form
+    public partial class frmPrincipal : Form
     {
-        public Form1()
+        public frmPrincipal()
         {
             InitializeComponent();
         }
